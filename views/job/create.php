a45d87d6aa3941fe428af180e9b9c7a48a27a71a
IgorTokman <igortokman1994@ukr.net>
2016-04-27 13:59:57 +0300

    created controllers and views with Gii

---
+++ views/job/create.php
@@ -0,0 +1,10 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1>job/create</h1>
+<h2><?=$foo?></h2>
+
+<p>
+    You may change the content of this page by modifying
+    the file <code><?= __FILE__; ?></code>.
+</p>
