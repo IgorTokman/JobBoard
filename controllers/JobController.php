c207e68a4400c16f5a7229463c7a6a8e37d1f2e1
IgorTokman <igortokman1994@ukr.net>
2016-04-28 01:05:58 +0300

    realized the mechanism of displaying jobs

--- controllers/JobController.php
+++ controllers/JobController.php
@@ -2,6 +2,14 @@
 
 namespace app\controllers;
 
+use Yii;
+use yii\data\Pagination;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use app\models\Category;
+use app\models\Job;
+
 class JobController extends \yii\web\Controller
 {
     public function actionCreate()
@@ -20,9 +28,30 @@ class JobController extends \yii\web\Controller
         return $this->render('edit');
     }
 
+    public function actionDetails()
+    {
+        return $this->render('details');
+    }
+
     public function actionIndex()
     {
-        return $this->render('index');
+        //Creates query
+        $query = Job::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 20,
+            'totalCount' => $query->count()
+        ]);
+
+        $jobs = $query->orderBy('create_date')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'pagination' => $pagination,
+            'jobs' => $jobs
+        ]);
     }
 
 }
--- views/job/index.php
+++ views/job/index.php
@@ -1,9 +1,32 @@
 <?php
 /* @var $this yii\web\View */
-?>
-<h1>job/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+?>
+
+<h1 class="page-header">
+    Jobs
+    <a href="index.php?r=job/create" class="btn btn-primary pull-right">Create</a>
+</h1>
+
+<?php if(null !== Yii::$app->session->getFlash('success')):?>
+    <div class="alert alert-success"><?=Yii::$app->session->getFlash('success')?></div>
+<?php endif;?>
+
+<?php if(!empty($jobs)) : ?>
+    <ul class="list-group">
+        <?php foreach ($jobs as $job):?>
+            <li class="list-group-item">
+                <a href="/index.php?r=job/details&id=<?= $job->id?>">
+                    <?= $job->title?> - <strong><?= $job->city?>  <?=$job->state?></strong> -
+                    Listed on <?=date("F j, Y, g:ia", strtotime($job->create_date))?>
+                </a>
+            </li>
+        <?php endforeach;?>
+    </ul>
+<?php else:?>
+    <p>No jobs to list</p>
+<?php endif; ?>
+
+<?= LinkPager::widget(['pagination' => $pagination])?>
